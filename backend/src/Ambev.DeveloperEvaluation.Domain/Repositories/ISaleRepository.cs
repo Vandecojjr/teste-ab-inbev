da248070b90fce06a58ee9ae3bc740783ca19d72
Vandecojjr <140183655+Vandecojjr@users.noreply.github.com>
2026-09-16 08:19:39 -0400

    feat: add flow with sale in infra layer

--- backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
+++ backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
@@ -7,7 +7,6 @@ public interface ISaleRepository
     Task<Sale> CreateAsync(Sale sale, CancellationToken cancellationToken = default);
     Task<Sale?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
     IQueryable<Sale> GetQueryable();
-    Task<int> GetTotalCountAsync(CancellationToken cancellationToken = default);
     Task<Sale> UpdateAsync(Sale sale, CancellationToken cancellationToken = default);
     Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
 }
